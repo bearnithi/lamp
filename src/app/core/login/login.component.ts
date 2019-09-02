d59d0a71f9e75e6e8e73885edeb8972a0c83e0ce
bearnithi <bearnithi@gmail.com>
2019-09-02 19:11:03 +0530

    lender dashboard design started

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,26 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   @Output() signup = new EventEmitter<boolean>();
-  constructor(private fb: FormBuilder) { }
+  showLoader: boolean;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
     this.loginForm  = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    this.showLoader = true;
+    setTimeout(() => {
+      this.showLoader = false;
+      this.router.navigate(['lender', 'lender-dashboard']);
+    }, 3000);
+
   }
 
 }
