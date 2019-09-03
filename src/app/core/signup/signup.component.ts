4c40cef8cb05dcb6aa7fde8436fecd054d7400c5
Ragav <ragavendrann@torus.tech>
2019-09-03 22:21:45 +0530

    Registration form completed

--- src/app/core/signup/signup.component.ts
+++ src/app/core/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
+
+
+import axios from "axios";
+import { AxiosInstance } from "axios";
 
 @Component({
   selector: 'app-signup',
@@ -8,22 +12,46 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
   signUpForm: FormGroup;
+  private axiosClient: AxiosInstance;
+  submitted = false;
+
   @Output() login = new EventEmitter<boolean>();
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.signUpForm = this.fb.group({
+      userType: ['', Validators.required],
       firstName: ['', Validators.required],
       middleName: ['', Validators.required],
       lastName: ['', Validators.required],
-      dob: ['', Validators.required],
-      gender: ['', Validators.required],
       mobileNo: ['', Validators.required],
-      emailID: ['', [Validators.required, Validators.email
-      ]],
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern('(?=\\D*\\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}')
+      ]]
     })
+
+    this.axiosClient = axios.create({
+      timeout: 3000,
+      headers: {
+        "X-Initialized-At": Date.now().toString()
+      }
+    });
+  }
+
+
+  get control() { return this.signUpForm.controls; }
+
+
+  registration(form: Form) {
+    this.axiosClient.post('http://192.168.2.189:43030/users', form['value'])
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
 }
