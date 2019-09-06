215cca7642a0ce6cd8faac010bc27f07cded2f58
Ragav <ragavendrann@torus.tech>
2019-09-06 13:20:28 +0530

    added image component

--- src/app/shared/add-product/cars/cars.component.ts
+++ src/app/shared/add-product/cars/cars.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { Location } from '@angular/common';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpHelperService } from 'src/app/services/http-helper.service';
 import { ValidationService } from 'src/app/services/validation.service';
@@ -17,6 +17,9 @@ import { ProductService } from 'src/app/services/product.service';
 export class CarsComponent implements OnInit {
 
   addCarDetailForm: FormGroup;
+  images: FormArray;
+
+  bindImages:any = [];
 
   constructor( public router: Router,
     private activatedRoute: ActivatedRoute,
@@ -47,8 +50,55 @@ export class CarsComponent implements OnInit {
       valuedBy:[''],
       mentions:[''],
       insuranceCompany:[''],
-      insuranceValidityEndDate:['']
+      insuranceValidityEndDate:[''],
+      images: this.fb.array([ this.createItem() ])
     });
+  }
+
+  createItem(): FormGroup {
+    return this.fb.group({
+      caption: '',
+      file: ''
+    });
+  }
+
+
+  deleteItem(i){
+    
+    this.images.removeAt(i)
+  }
+
+  addItem(): void {
+    this.images = this.addCarDetailForm.get('images') as FormArray;
+    this.images.push(this.createItem());
+  }
+
+
+  handleFileUpload(e,index) {
+    const file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    const pattern = /image-*/;
+
+    let temp = "";
+
+    const reader = new FileReader();
+
+    if (!file.type.match(pattern)) {
+      return 'invalid format';
+    }
+
+    reader.onload = event => {
+      const fReader: any = event.target;
+      temp = fReader.result;
+
+      this.addCarDetailForm.value.images[index]['file'] = temp;
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+
+  getImageUrl(index){
+    return this.addCarDetailForm.value.images[index]['file'];
   }
 
   get control(): any { return this.addCarDetailForm.controls; }
